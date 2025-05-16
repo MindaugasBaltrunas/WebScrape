bd7e63f857292305a9f13b0c487c20c5a9655117
minde8888 <58484005+minde8888@users.noreply.github.com>
2025-05-16 16:51:27 +0200

    update

--- src/WebScrape.Services/obj/Debug/net9.0/WebScrape.Services.AssemblyInfo.cs
+++ src/WebScrape.Services/obj/Debug/net9.0/WebScrape.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebScrape.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4e5d3a82942a7f1c8f42fdb1a080af94a58896d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6c0552164fdfff5f436f788df2c6c8e87277ad6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebScrape.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebScrape.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
